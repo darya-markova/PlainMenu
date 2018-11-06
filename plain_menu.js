686796aac4447c515ad0e4949a65048f62e26528
markova <markova@reksoft.ru>
2018-11-06 14:40:19 +0300

    wrapping prev and next elements into div

--- plain_menu.js
+++ plain_menu.js
@@ -1,103 +1,107 @@
 $(document).ready(function() {
-
     $('.plain-menu .item').mouseover(function() {
+
         if ($(this).hasClass('animated')) {
             return;
         }
 
         var item = $(this);
-        var number = item.index();
-        var prevCount = item.prevAll().length;
 
         item.addClass('animated');
 
-        item.prevAll().each(function(index) {
-            $(this).animate({
-                left: (prevCount - 1 - index) * 50 - 25
-            }, {
-                duration: 100,
-                queue: false
-            });
-        });
-        item.nextAll().each(function(index) {
-            $(this).animate({
-                left: (prevCount + index + 1)* 50 + 25
-            }, {
-                duration: 100,
-                queue: false
-            });
+        var number = item.index();
+
+        var prevAll = item.prevAll();
+        var nextAll = item.nextAll();
+
+        var prevCount = prevAll.length;
+        var nextCount = nextAll.length;
+
+        var prevWrapper = $("<div class='prev-elems'></div>");
+        prevWrapper.css({
+            position: 'absolute',
+            left: 0,
+            top: 25,
+            height: 50,
+            width: prevCount * 52
         });
 
-        item.animate({
+        prevAll.addClass('previous');
+        $('.item.previous').wrapAll(prevWrapper);
+
+        var nextWrapper = $("<div class='next-elems'></div>");
+        nextWrapper.css({
+            position: 'absolute',
+            left: item.position().left + item.width(),
+            top: 25,
+            height: 50,
+            width: nextCount * 52
+        });
+
+        nextAll.addClass('following');
+
+        var followElems = $('.item.following');
+        var prevElems =  $('.item.previous');
+
+        followElems.wrapAll(nextWrapper);
+
+        followElems.each(function(index) {
+            $(this).css({left: index*52});
+        });
+
+        item.css({
+            width: 100,
+            height: 100,
+            top: 0,
+            left: number * 52 - 26
+        });
+        /*item.animate({
             width: '100px',
             height: '100px',
             top: '0px',
-            left: number * 50 - 25
+            left: number * 52 - 26
         }, {
             duration: 100,
             queue: false
         });
-    });
 
-    $('.plain-menu').mouseleave(function() {
-        //reset animation
-        $('.item').each(function() {
-            var item = $(this);
-            var number = item.index();
-
-            item.animate({
-                width: '50px',
-                height: '50px',
-                top: '25px',
-                left: number * 50
-            }, {
-                duration: 50,
-                queue:false
-            });
+        prevElems.animate({
+            left: '-=26px'
+        }, {
+            duration: 100,
+            queue: false
         });
+
+        followElems.animate({
+            left: '+=26px'
+        }, {
+            duration: 100,
+            queue: false
+        })*/
     });
 
-    var prevItem;
     $('.plain-menu .item').mouseleave(function() {
         var item = $(this);
-        var number = item.index();
+
+        $('.prev-elems').contents().unwrap();
+        $('.next-elems').contents().unwrap();
+
+        item.prevAll().removeClass('previous');
+        item.nextAll().removeClass('following');
 
         var prevCount = item.prevAll().length;
-        prevItem = item;
 
-        item.clearQueue().stop();
-        item.removeClass('animated');
-
-        item.prevAll().clearQueue().finish();
-        item.nextAll().clearQueue().finish();
-
-        item.prevAll().each(function(index) {
-            $(this).animate({
-                left: (prevCount - 1 - index) * 50
-            }, {
-                duration: 50,
-                queue: false
-            });
+        item.css({
+            width: 50,
+            height: 50,
+            left: prevCount * 52,
+            top: 25
         });
 
         item.nextAll().each(function(index) {
-            $(this).animate({
-                left: (prevCount + index + 1)* 50
-            }, {
-                duration: 50,
-                queue: false
-            });
+            $(this).css({left: item.position().left + item.outerWidth() + 2 + index*52});
         });
 
-        item.animate({
-            width: '50px',
-            height: '50px',
-            top: '25px',
-            left: number * 50
-        }, {
-            duration: 50,
-            queue:false
-        });
+       item.removeClass('animated');
     });
 });
-//Идея - оборачивать предыдущие и последующие блоки
