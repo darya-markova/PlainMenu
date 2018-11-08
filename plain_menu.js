b085a2b0b71292e4ce35b504bb1014180b318e19
markova <markova@reksoft.ru>
2018-11-08 16:02:06 +0300

    adding gradient borders

--- plain_menu.js
+++ plain_menu.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
+    var plainMenu = $('.plain-menu');
+    var size = 50;
+
     $('.plain-menu .item').mouseover(function() {
 
         var item = $(this);
-        var items = $('.plain-menu').find('.item');
+        var items = plainMenu.find('.item');
         var itemIndex = item.index();
 
         items.clearQueue().stop();
 
-        var i;
+        var i, leftOffset;
         for (i = 0; i < itemIndex; i++) {
-            var leftOffset = i * (50 + 2) - 25;
+            leftOffset = i * (size + 2) - 25;
+
             $('.item').eq(i).animate({
                 left: leftOffset,
                 top: 25,
-                height: 50,
-                width: 50
+                height: size,
+                width: size
             }, {
                 duration: 100,
                 queue: false
@@ -22,23 +26,24 @@ $(document).ready(function() {
         }
 
         for (i = items.length - 1; i > itemIndex; i--) {
-            var leftOffset = i * (50 + 2) + 25;
+            leftOffset = i * (size + 2) + 25;
+
             $('.item').eq(i).animate({
                 left: leftOffset,
                 top: 25,
-                height: 50,
-                width: 50
+                height: size,
+                width: size
             }, {
                 duration: 100,
                 queue: false
             });
         }
 
-        var itemLeft = item.index() * (50 + 2) - 25;
+        var itemLeft = item.index() * (size + 2) - 25;
 
         item.animate({
-            width: 100,
-            height: 100,
+            width: 2*size,
+            height: 2*size,
             top: 0,
             left: itemLeft
         }, {
@@ -49,14 +54,14 @@ $(document).ready(function() {
 
     $('.plain-menu .item').mouseleave(function() {
 
-        var items = $('.plain-menu').find('.item');
-        $('.item').clearQueue().stop();
+        var items = plainMenu.find('.item');
+        items.clearQueue().stop();
 
-        var i;
+        var i, leftOffset;
         for (i = 0; i < items.length; i++) {
             $('.item').eq(i).animate({
-                height: 50,
-                width: 50
+                height: size,
+                width: size
             }, {
                 duration: 100,
                 queue: false
@@ -64,10 +69,13 @@ $(document).ready(function() {
         }
 
         for (i = 0; i < items.length; i++) {
-            var leftOffset = i * (50 + 2);
+            leftOffset = i * (size + 2);
+
             $('.item').eq(i).animate({
                 top: 25,
-                left: leftOffset
+                left: leftOffset,
+                /*height: 50,
+                width: 50*/
             }, {
                 duration: 100,
                 queue: false
