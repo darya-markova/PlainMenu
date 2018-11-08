058e878a773bf4a61b506047d999563c7e022a6d
markova <markova@reksoft.ru>
2018-11-08 14:12:21 +0300

    the best version with smooth transactions

--- plain_menu.js
+++ plain_menu.js
@@ -1,114 +1,77 @@
 $(document).ready(function() {
-    //сделать длительность пропорциональную числу блокам
     $('.plain-menu .item').mouseover(function() {
-        if ($(this).hasClass('animated')) {
-            return;
+
+        var item = $(this);
+        var items = $('.plain-menu').find('.item');
+        var itemIndex = item.index();
+
+        items.clearQueue().stop();
+
+        var i;
+        for (i = 0; i < itemIndex; i++) {
+            var leftOffset = i * (50 + 2) - 25;
+            $('.item').eq(i).animate({
+                left: leftOffset,
+                top: 25,
+                height: 50,
+                width: 50
+            }, {
+                duration: 100,
+                queue: false
+            });
         }
 
-        var item = $(this);
+        for (i = items.length - 1; i > itemIndex; i--) {
+            var leftOffset = i * (50 + 2) + 25;
+            $('.item').eq(i).animate({
+                left: leftOffset,
+                top: 25,
+                height: 50,
+                width: 50
+            }, {
+                duration: 100,
+                queue: false
+            });
+        }
 
-        item.addClass('animated');
+        var itemLeft = item.index() * (50 + 2) - 25;
 
-        //var number = item.index();
-
-        var prevAll = item.prevAll();
-        var nextAll = item.nextAll();
-
-        var prevCount = prevAll.length;
-        var nextCount = nextAll.length;
-
-        var prevWrapper = $("<div class='prev-elems'></div>");
-
-        prevWrapper.css({
-            position: 'absolute',
-            left: 0,
-            top: 25,
-            height: 50,
-            width: prevCount * 52
-        });
-        prevAll.addClass('previous');
-        var prevElems =  $('.item.previous');
-        prevElems.wrapAll(prevWrapper);
-
-        var nextWrapper = $("<div class='next-elems'></div>");
-        nextWrapper.css({
-            position: 'absolute',
-            left: item.position().left + item.width(),
-            top: 25,
-            height: 50,
-            width: nextCount * 52
-        });
-        nextAll.addClass('following');
-
-        var followElems = $('.item.following');
-        followElems.wrapAll(nextWrapper);
-
-
-        followElems.css({top: 0});
-        prevElems.css({top: 0});
-
-        followElems.each(function(index) {
-            $(this).css({left: index*52});
-        });
-
-        item.css({
+        item.animate({
             width: 100,
             height: 100,
-            left: '-=24px',
-            top: 0
-        });
-
-
-        $('.prev-elems').animate({
-            left: -26
-        }, {
-            duration: 200,
-            queue: false
-        });
-
-        $('.next-elems').animate({
-            left:  prevElems.children().length * 52 + 52 + 26
-        }, {
-            duration: 200,
-            queue: false
-        });
-    });
-
-    $('.plain-menu .item').mouseleave(function() {
-        var item = $(this);
-
-        var offset = $('.prev-elems').children().length * 52;
-
-        $('.prev-elems').clearQueue().stop();
-        $('.next-elems').clearQueue().stop();
-        item.clearQueue().stop();
-
-        item.css({
-            width: 50,
-            height: 50,
-            left: '+=24px',
-            top: 25
-        });
-
-
-        $('.prev-elems').animate({
-            left: 0
+            top: 0,
+            left: itemLeft
         }, {
             duration: 100,
             queue: false
-        });
+        })
+    });
 
-        $('.prev-elems').contents().unwrap();
-        $('.next-elems').contents().unwrap();
+    $('.plain-menu .item').mouseleave(function() {
 
+        var items = $('.plain-menu').find('.item');
+        $('.item').clearQueue().stop();
 
-        item.prevAll().css({top: 25}).removeClass('previous');
-        item.nextAll().css({top: 25}).removeClass('following');
+        var i;
+        for (i = 0; i < items.length; i++) {
+            $('.item').eq(i).animate({
+                height: 50,
+                width: 50
+            }, {
+                duration: 100,
+                queue: false
+            });
+        }
 
-        item.nextAll().each(function(index) {
-            $(this).css({left: offset + 52 + index*52});
-        });
-
-        item.removeClass('animated');
+        for (i = 0; i < items.length; i++) {
+            var leftOffset = i * (50 + 2);
+            $('.item').eq(i).animate({
+                top: 25,
+                left: leftOffset
+            }, {
+                duration: 100,
+                queue: false
+            });
+        }
     });
 });
